0b8c36d726819289c08b457735191476ca6234c6
keshavkourav <keshavkourav107@gmail.com>
2025-11-02 00:11:55 +0530

    small fix

---
+++ problems_on_arrays/q2.java
@@ -0,0 +1,17 @@
+// Given an array of integers nums, return the value of the largest element in the array
+
+
+
+class Solution {
+    public int largestElement(int[] nums) {
+        int largest=nums[0];
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>largest){
+                largest=nums[i];
+            }
+        }
+        return largest;
+    
+    }
+}
